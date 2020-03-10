602a9523c4bd072ae93a12266f05442661ee19cc
Praveen Kumar.V <55880770+praveenkumar0211@users.noreply.github.com>
2020-03-10 18:28:39 +0530

    final code

--- assignment5/chat-server.c
+++ assignment5/chat-server.c
@@ -1,46 +1,48 @@
-# include <sys/ipc.h>
-# define  NULL 0
-# include <sys/shm.h>
-# include <sys/types.h>
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <sys/wait.h>
-# include <stdio_ext.h>
-# include <errno.h>
-# include <fcntl.h>
-# include <ctype.h>
-# define  BUFF_SIZE 1024
-
+#include<sys/ipc.h>
+#define NULL 0
+#include<sys/shm.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/wait.h>
 int main()
 {
-	char *avail,*usr1,*usr2;
-	int id,id1,id2;
-	
-	id=shmget(111,50,IPC_CREAT | 00666);
-	id1=shmget(112,50,IPC_CREAT | 00666);
-	id2=shmget(113,50,IPC_CREAT | 00666);
-	
-	avail=shmat(id,NULL,0);
-	usr1=shmat(id1,NULL,0);
-	usr2=shmat(id2,NULL,0);
-	strcpy(usr1,"-1");
-	strcpy(usr2,"-1");
-	while(1){
-	
-		printf("You      : ");
-		scanf (" %[^\n]",usr1);
-	
-		if(strcmp(usr2,"-1")!=0){
-			printf("Client 2 : %s\n",usr2);
-			strcpy(usr2,"-1");
-		}
-		
-		if(strcmp(usr1,"exit")==0){
-			shmdt(usr1);
-			shmctl(id1, IPC_RMID,NULL);
-			break;
-		}		
-	}
+int id;
+char *a;
+char temp[50],t1[50];
+char s[50];
+strcpy(s,"*");
+id = shmget(115, 50, IPC_CREAT | 00666);
+a = shmat(id, NULL, 0);
+printf("You: ");
+scanf(" %[^\n]",temp);
+strcat(s,temp);
+strcpy(a,s);
+strcpy(s,"*");
+while(1) {
+while(a[0]=='*');
+strcpy(t1,a);
+char *t = strtok(t1,"#");
+printf("Friend: %s\n",t);
+if(strcmp(t,"bye")==0)
+{
+strcpy(temp,"bye");
+printf("You: %s ",temp);
+sleep(5);
+break;
+}
+printf("You: ");
+scanf(" %[^\n]",temp);
+strcat(s,temp);
+strcpy(a,s);
+strcpy(s,"*");
+if(strcmp(temp,"bye")==0)
+{
+sleep(5);
+break;
+}
+}
+printf("Connection Ended!\n");shmdt(a);
 }
